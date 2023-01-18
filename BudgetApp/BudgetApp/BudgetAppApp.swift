8439a7b4fba7a962817790b077979ce9d9114149
Beau Nouvelle <beau@getswifty.dev>
2023-01-19 10:05:13 +1100

    Added Budget App

---
+++ BudgetApp/BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Beau Nouvelle on 19/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
