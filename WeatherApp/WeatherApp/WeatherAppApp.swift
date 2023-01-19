7dd00cf2da60c91fbef68a9c89e863e0c66ba65e
Beau Nouvelle <beau@getswifty.dev>
2023-01-20 08:42:45 +1100

    Started work on weather app

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Beau Nouvelle on 20/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
